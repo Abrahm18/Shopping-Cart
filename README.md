f5b22596acd2fe4707195c5638f0d9a0b667817a
VxneC0de <vanerubio2020@gmail.com>
2024-04-15 13:43:29 -0400

    fix Archivos Global Templates README.md index.php

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
-# Shopping-Cart
+![](https://img.shields.io/badge/Uneweb-blue)
+
+#Shopping-Cars
+
+> escribimos algo 
+
+![screenshot]()
+
+ escribimos algo
+
+## Built with
+
+- php
+- VSCode
+- Git Bash
+
+## Live Demo
+
+[Live Demo Link](montarlo)
+
+
+## Starting
+
+**here you will learn a little about what flebox is, it will be from one moment to the next it will be difficult you will see why I say it**
+
+
+To get a local copy up and running, follow these simple example steps.
+
+### Previous requirements
+
+### Setting
+
+### Install
+
+### Use
+
+### Run tests
+
+### Implementation
+
+
+
+## Authors
+
+👤 **Abraham Nazareth Hidalgo Sanoja**
+
+- GitHub: [@abrahm18](https://github.com/abrahm18)
+
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check out the [issues page](https://github.com/Abrahm18/odin-recipes/issues).
+
+## Show your support
+
+Give it a ⭐️ if you like this project!
+
+## Thanks
+
+- I thank Google for giving me the information I needed to carry out this project.
+- I thank my partner Daniel who helped me complete my project and for teaching me some tricks
+
+## 📝 License
+
+This project is [CC0 1.0 Universal](LICENSE) licensed.
---
+++ index.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shopping Cars</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    
+ <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+    <a class="navbar-brand" href="index.php">Logo</a>
+    <button class="navbar-toggler" data-target="#my-nav" data-toggle="collapse" aria-controls="my-nav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div id="my-nav" class="collapse navbar-collapse">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Car(0)</a>
+            </li>
+        </ul>
+    </div>
+ </nav>
+<br/>
+<br/>
+<div class="container">
+<br>
+<div class="alert alert-primary" role="alert">
+    Pantalla de mensaje...
+    <a href="#" class="badge badge-sucess">ver carrito</a>  
+</div>    
+<div class="row">
+    <div class="col-3">
+    <div class="card">
+        <img 
+        title="God oF War"
+        class="card-img-top" src="https://images.gog-statics.com/373a2f94eb35e1fb653999a3620c78d94f17d9db50191596a61c8a566374e3b7_product_card_v2_mobile_slider_639.jpg" alt="Juego">
+        <div class="card-body">
+            <span>God of War Game for PS5</span>
+            <h5 class="card-title">$95</h5>
+            <p class="card-text">descripcion</p>
+            <button class="btn btn-primary" 
+            type="submit" 
+            name="btnAccion" 
+            value="Agregar" >Add to cart</button>
+        </div>
+    </div>
+    </div>
+    <div class="col-3">
+    <div class="card">
+        <img 
+        title="God oF War"
+        class="card-img-top" src="https://images.gog-statics.com/373a2f94eb35e1fb653999a3620c78d94f17d9db50191596a61c8a566374e3b7_product_card_v2_mobile_slider_639.jpg" alt="Juego">
+        <div class="card-body">
+            <span>God of War Game for PS5</span>
+            <h5 class="card-title">$95</h5>
+            <p class="card-text">descripcion</p>
+            <button class="btn btn-primary" 
+            type="submit" 
+            name="btnAccion" 
+            value="Agregar" >Add to cart</button>
+        </div>
+    </div>
+    </div>
+    <div class="col-3">
+    <div class="card">
+        <img 
+        title="God oF War"
+        class="card-img-top" src="https://images.gog-statics.com/373a2f94eb35e1fb653999a3620c78d94f17d9db50191596a61c8a566374e3b7_product_card_v2_mobile_slider_639.jpg" alt="Juego">
+        <div class="card-body">
+            <span>God of War Game for PS5</span>
+            <h5 class="card-title">$95</h5>
+            <p class="card-text">descripcion</p>
+            <button class="btn btn-primary" 
+            type="submit" 
+            name="btnAccion" 
+            value="Agregar" >Add to cart</button>
+        </div>
+    </div>
+    </div>
+    <div class="col-3">
+    <div class="card">
+        <img 
+        title="God oF War"
+        class="card-img-top" src="https://images.gog-statics.com/373a2f94eb35e1fb653999a3620c78d94f17d9db50191596a61c8a566374e3b7_product_card_v2_mobile_slider_639.jpg" alt="Juego">
+        <div class="card-body">
+            <span>God of War Game for PS5</span>
+            <h5 class="card-title">$95</h5>
+            <p class="card-text">descripcion</p>
+            <button class="btn btn-primary" 
+            type="submit" 
+            name="btnAccion" 
+            value="Agregar" >Add to cart</button>
+        </div>
+    </div>
+    </div>
+    <div class="col-3">
+    <div class="card">
+        <img 
+        title="God oF War"
+        class="card-img-top" src="https://images.gog-statics.com/373a2f94eb35e1fb653999a3620c78d94f17d9db50191596a61c8a566374e3b7_product_card_v2_mobile_slider_639.jpg" alt="Juego">
+        <div class="card-body">
+            <span>God of War Game for PS5</span>
+            <h5 class="card-title">$95</h5>
+            <p class="card-text">descripcion</p>
+            <button class="btn btn-primary" 
+            type="submit" 
+            name="btnAccion" 
+            value="Agregar" >Add to cart</button>
+        </div>
+    </div>
+    </div>
+    <div class="col-3">
+    <div class="card">
+        <img 
+        title="God oF War"
+        class="card-img-top" src="https://images.gog-statics.com/373a2f94eb35e1fb653999a3620c78d94f17d9db50191596a61c8a566374e3b7_product_card_v2_mobile_slider_639.jpg" alt="Juego">
+        <div class="card-body">
+            <span>God of War Game for PS5</span>
+            <h5 class="card-title">$95</h5>
+            <p class="card-text">descripcion</p>
+            <button class="btn btn-primary" 
+            type="submit" 
+            name="btnAccion" 
+            value="Agregar" >Add to cart</button>
+        </div>
+    </div>
+    </div>
+    <div class="col-3">
+    <div class="card">
+        <img 
+        title="God oF War"
+        class="card-img-top" src="https://images.gog-statics.com/373a2f94eb35e1fb653999a3620c78d94f17d9db50191596a61c8a566374e3b7_product_card_v2_mobile_slider_639.jpg" alt="Juego">
+        <div class="card-body">
+            <span>God of War Game for PS5</span>
+            <h5 class="card-title">$95</h5>
+            <p class="card-text">descripcion</p>
+            <button class="btn btn-primary" 
+            type="submit" 
+            name="btnAccion" 
+            value="Agregar" >Add to cart</button>
+        </div>
+    </div>
+    </div>
+</div>
+</div>
+</body>
+</html>
